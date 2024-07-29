cab136909d0832bfce67c473fa57ecf8eb397218
rteran <richard_teran@contraloria.gob.bo>
2024-07-28 22:44:29 -0400

    docs: hasta interpolacion

--- React/proyectos-react-vite/fundamentos-react/src/App.jsx
+++ React/proyectos-react-vite/fundamentos-react/src/App.jsx
@@ -1,8 +1,15 @@
-//- para llevarlo a otro lugar
-//- Las exportaciones por default solo se puede llevar una cosa
-//- En el destino js sabe que vendra una sola cosa, por ello si se quiere se puede llamar de otra forma
 const App = () => {
-  return <h1>Hola soy react</h1>;
+
+  const title = 'Mi titulo desde una constante'
+  const classTitle = 'text-center'
+  const pathImg = 'https://picsum.photos/200/300'
+
+  return (
+    <>
+      <h1 className={classTitle}>{title}</h1>
+      <img src={pathImg} alt={`imagen-${title}`} />
+    </>
+  )
 };
 
-export default App
+export default App;
