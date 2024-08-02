bca64b21aaf24a2673de92fc77eb91b4bb5653e8
rteran <richard_teran@contraloria.gob.bo>
2024-08-01 23:02:33 -0400

    fix:usando primeReact

--- React+vite-Formularios/formularios-react/src/App.jsx
+++ React+vite-Formularios/formularios-react/src/App.jsx
@@ -1,9 +1,11 @@
 import Nocontrolado from './components/NoControlado';
+import { Button } from 'primereact/button';
 
 const App = () => {
   return (
     <>
       <Nocontrolado />
+      <Button label="Click Me" icon="pi pi-check" />
     </>
   );
 };
